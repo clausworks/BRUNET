7b0d4bf3c5909e168f4a64bc9e8ffc451c0943ce
clausworks <clausworks@icloud.com>
2023-10-21 11:01:34 -0700

    Create leave/join scripts

---
+++ joinpifi.sh
@@ -0,0 +1,7 @@
+#sudo iw wlan0 set type ibss
+#sudo ip link set wlan0 up
+sudo iw wlan0 ibss join "PiFi" 2432
+
+#sudo batctl if add wlan0
+#sudo ifconfig wlan0 up
+#sudo ifconfig bat0 up
---
+++ leavepifi.sh
@@ -0,0 +1,7 @@
+#sudo iw wlan0 set type ibss
+#sudo ip link set wlan0 up
+sudo iw wlan0 ibss leave
+
+#sudo batctl if add wlan0
+#sudo ifconfig wlan0 up
+#sudo ifconfig bat0 up
