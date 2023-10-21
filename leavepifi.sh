d2aa7ae67f4af4776b46b61e5969b521b262c210
clausworks <clausworks@zoho.com>
2023-10-21 14:52:16 -0700

    Add iw wlan0 info

--- leavepifi.sh
+++ leavepifi.sh
@@ -1,6 +1,9 @@
 #sudo iw wlan0 set type ibss
 #sudo ip link set wlan0 up
 sudo iw wlan0 ibss leave
+sudo iw wlan0 ibss join "FakeFi" 2432
+sleep 3
+iw wlan0 info
 
 #sudo batctl if add wlan0
 #sudo ifconfig wlan0 up
