312eecdd913ea45cb0ca0b1c1b2a5b2acc921f44
clausworks <clausworks@zoho.com>
2023-10-25 14:36:32 -0700

    Update test files

--- testfiles/clnt1.py
+++ testfiles/clnt1.py
@@ -1,11 +1,9 @@
 import socket
+import sys
 
 HOST = "10.0.0.2"  # The server's hostname or IP address
-PORT = 2345  # The port used by the server
+PORT = 1234  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(input().encode())
-    #data = s.recv(1024)
-
-#print(f"Received {data!r}")
+    s.sendall(sys.stdin.buffer.read())
