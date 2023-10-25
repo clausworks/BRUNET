2554648d35eff0a7cfbd58afb1e76f4d86678c7d
clausworks <clausworks@zoho.com>
2023-10-25 13:16:57 -0700

    clnt1 is sendfile

--- testfiles/clnt1.py
+++ testfiles/clnt1.py
@@ -5,7 +5,7 @@ PORT = 2345  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    s.sendall(input().encode())
+    #data = s.recv(1024)
 
-print(f"Received {data!r}")
+#print(f"Received {data!r}")
