6bea0f9a5a71fdc3436fe199e0855a242a146a55
clausworks <clausworks@icloud.com>
2023-10-25 13:17:24 -0700

    Serv1 prints data

--- testfiles/serv1.py
+++ testfiles/serv1.py
@@ -1,6 +1,7 @@
 # echo-server.py
 
 import socket
+import sys
 
 HOST = "10.0.0.2"
 PORT = 2345  # Port to listen on (non-privileged ports are > 1023)
@@ -13,6 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
+            sys.stdout.buffer.write(data);
             if not data:
                 break
-            conn.sendall(data)
+            #conn.sendall(data)
