165675a069db81ac85bf51f15ba84b71ceb4ee26
clausworks <clausworks@icloud.com>
2023-11-02 21:26:50 -0700

    Fix bug

--- testfiles/config/prepend_this_dev.sh
+++ testfiles/config/prepend_this_dev.sh
@@ -5,6 +5,8 @@ fi
 file_prefix=$1
 this_dev_addr=$2
 
+rm _${file_prefix}_*.yaml
+
 for file in *.yaml; do
     newfile=_${file_prefix}_${file}
     printf "this_device: ${this_dev_addr}\n\n" | cat - "$file" > "$newfile"
