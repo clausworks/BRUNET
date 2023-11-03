01d9440b09983a38ac3880bb8be53825579fb519
clausworks <clausworks@zoho.com>
2023-11-02 20:34:26 -0700

    Turn on LOG_DEBUG

--- main.c
+++ main.c
@@ -3052,8 +3052,8 @@ int main(int argc, char **argv) {
     return 0;
 #else
 
-    //set_global_log_level(LOG_DEBUG);
-    set_global_log_level(LOG_INFO);
+    set_global_log_level(LOG_DEBUG);
+    //set_global_log_level(LOG_INFO);
 
     init_sighandlers();
     cache_global_init();
